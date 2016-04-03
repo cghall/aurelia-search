f37932337ddc61162ea7c6d7a6460f26e0bd4255
Chris Hall <christopherhall17@gmail.com>
2016-04-03 23:59:43 +0100

    Delete old unit tests and update app unit tests

--- src/search-bar.js
+++ src/search-bar.js
@@ -5,9 +5,3 @@ export class SearchBar {
   heading = 'Welcome to the Aurelia Search App';
   @bindable() filterText = '';
 }
-
-export class UpperValueConverter {
-  toView(value) {
-    return value && value.toUpperCase();
-  }
-}
